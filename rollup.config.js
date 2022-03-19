0656680c13360fa75bac91275beeb24f4ed08df3
Omkar Rajwade <orajwade@gmail.com>
2022-03-19 18:52:36 +0530

    rollup output include

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,10 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
-import serve from "rollup-plugin-serve";
-
 export default {
   input: "script.js",
   output: {
     dir: "output",
     format: "cjs",
   },
-  plugins: [nodeResolve(), commonjs(), livereload(), serve()],
+  plugins: [nodeResolve(), commonjs()],
 };
